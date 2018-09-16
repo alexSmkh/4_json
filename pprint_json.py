0de83b47f4ee4ad687a3043969f8d8ec3c39d8bd
alex <alexshikari9333@gmail.com>
2018-09-16 13:42:22 +0300

    Pretty print script created

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,17 @@
 import json
+from sys import argv
 
 
-def load_data(filepath):
-    pass
+def load_data(file_path):
+    with open(file_path, 'r', encoding='utf8') as file_handler:
+        return json.load(file_handler)
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=5, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pass
+    file_path = input('Укажите путь к файлу: ')
+    data = load_data(file_path)
+    pretty_print_json(data)
