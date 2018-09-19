5743db9e3cde0eaf2dbf27a8c931a9a8cc77b9eb
alex <alexshikari9333@gmail.com>
2018-09-20 00:26:01 +0300

    fixed: changed pretty_print_json

--- pprint_json.py
+++ pprint_json.py
@@ -4,25 +4,12 @@ from json.decoder import JSONDecodeError
 
 
 def load_data(file_name):
-    try:
-        with open(file_name, 'r', encoding='utf-8-sig') as file_handler:
-            return json.load(file_handler)
-    except IOError:
-        print_info_about_error('IOError')
-    except JSONDecodeError:
-        print_info_about_error('JSONDecodeError')
+    with open(file_name, 'r', encoding='utf8') as file_handler:
+        return json.load(file_handler)
 
 
 def pretty_print_json(data_for_formatting):
-    return json.dumps(data_for_formatting, indent=5, sort_keys=True, ensure_ascii=False)
-
-
-def print_info_about_error(exception):
-    probable_exceptions = {
-        'IndexError': 'You forgot to enter the file name.',
-        'IOError': 'File not found.',
-        'JSONDecodeError': 'The file must be a json format.'}
-    print(probable_exceptions.get(exception))
+    print(json.dumps(data_for_formatting, indent=5, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
@@ -30,6 +17,10 @@ if __name__ == '__main__':
         file_path = sys.argv[1]
         data_from_file = load_data(file_path)
         if data_from_file:
-            print(pretty_print_json(data_from_file))
+            pretty_print_json(data_from_file)
     except IndexError:
-        print_info_about_error('IndexError')
+        print('You forgot to enter the file name.')
+    except IOError:
+        print('File not found')
+    except JSONDecodeError:
+        print('The file must be a json format')
