5e9e34ae8ee741776f97a9fbaa84b8cc6efa69a4
alex <alexshikari9333@gmail.com>
2018-09-19 23:54:02 +0300

    fixed:add function for print info about errors

--- pprint_json.py
+++ pprint_json.py
@@ -4,12 +4,25 @@ from json.decoder import JSONDecodeError
 
 
 def load_data(file_name):
-    with open(file_name, 'r', encoding='utf-8-sig') as file_handler:
-        return json.load(file_handler)
+    try:
+        with open(file_name, 'r', encoding='utf-8-sig') as file_handler:
+            return json.load(file_handler)
+    except IOError:
+        print_info_about_error('IOError')
+    except JSONDecodeError:
+        print_info_about_error('JSONDecodeError')
 
 
 def pretty_print_json(data_for_formatting):
     return json.dumps(data_for_formatting, indent=5, sort_keys=True, ensure_ascii=False)
+
+
+def print_info_about_error(exception):
+    probable_exceptions = {
+        'IndexError': 'You forgot to enter the file name.',
+        'IOError': 'File not found.',
+        'JSONDecodeError': 'The file must be a json format.'}
+    print(probable_exceptions.get(exception))
 
 
 if __name__ == '__main__':
@@ -19,8 +32,4 @@ if __name__ == '__main__':
         if data_from_file:
             print(pretty_print_json(data_from_file))
     except IndexError:
-        print('You forgot to enter the file name.')
-    except IOError:
-        print('File not found.')
-    except JSONDecodeError:
-        print('The file must be a json format.')
+        print_info_about_error('IndexError')
